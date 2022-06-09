7fffa40ca7519950897a471ccacbe734ddb1d2f4
Bimsuru <47709078+Bimsuru@users.noreply.github.com>
2022-06-10 01:58:41 +0530

    error handling done

--- Client/src/app/layout/App.tsx
+++ Client/src/app/layout/App.tsx
@@ -4,13 +4,16 @@ import Header from "./Header";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import {Route} from "react-router-dom"
+import {Route, Switch} from "react-router-dom"
 import HomePage from "../../features/home/HomePage";
 import ProductDetails from "../../features/catalog/ProductDetails";
 import AboutPage from "../../features/about/AboutPage";
 import ContactPage from "../../features/contact/ContactPage";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css'
+import ServerError from "../errors/ServerError";
+import NotFound from "../errors/NotFound";
+ 
 
 function App() {
 
@@ -35,11 +38,17 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container>
+        <Switch>
+
         <Route exact path="/" component={HomePage}/> 
         <Route exact path='/catalog' component={Catalog}/>
         <Route path='/catalog/:id' component={ProductDetails}/>
         <Route path='/about' component={AboutPage}/>
         <Route path='/contact' component={ContactPage} />
+        <Route path='/server-error' component={ServerError} />
+        <Route component={NotFound} />
+        </Switch>
+
       </Container>
     </ThemeProvider>
   );
